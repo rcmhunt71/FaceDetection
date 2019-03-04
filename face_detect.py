b5830f117c7b235f1a6fbddebc8c0a388690e1af
robe6162 <chris.hunt@rackspace.com>
2019-03-04 14:27:21 -0600

    Added ability to select color for image match highlighting * Added RED, BLUE, GREEN option * Refactored code to provide color tuple (B, G, R)

--- face_detect.py
+++ face_detect.py
@@ -18,31 +18,41 @@ PROFILE_FACE_XML = 'haarcascade_profileface.xml'
 FRONTAL_FACE = os.path.sep.join([CLASSIFIER_PATH, FRONTAL_FACE_XML])
 PROFILE_FACE = os.path.sep.join([CLASSIFIER_PATH, PROFILE_FACE_XML])
 
+COLORS = {
+    'BLUE': (255, 0, 0),
+    'GREEN': (0, 255, 0),
+    'RED':  (0, 0, 255),
+}
+
 
 def get_cli_args():
     arguments = argparse.ArgumentParser()
     arguments.add_argument("image", )
     arguments.add_argument("--profile", '-p', help="Detect Profile Faces", default=False, action='store_true')
     arguments.add_argument("--frontal", '-f', help="Detect Frontal Faces", default=True, action='store_true')
+    arguments.add_argument("--color", '-c', help="Color used for highlighting matches",
+                           default='GREEN', choices=COLORS.keys())
 
     return arguments.parse_args()
 
 
-def highlight_cascade_matches(matches):
+def highlight_cascade_matches(matches, color):
+    if color not in COLORS.keys():
+        color = COLORS.keys()[0]
     for (column, row, width, height) in matches:
         cv.rectangle(original_image,
                      (column, row),
                      (column + width, row + height),
-                     (0, 255, 0), 2)
+                     COLORS.get(color), 2)
 
 
-def analyze_image(analysis, image, classifier):
+def analyze_image(analysis, image, classifier, color):
     check_msg = "Checking image for {classification_type}..."
 
     print(check_msg.format(classification_type=analysis))
     frontal_face_cascade = cv.CascadeClassifier(classifier)
     matches = frontal_face_cascade.detectMultiScale(image)
-    highlight_cascade_matches(matches)
+    highlight_cascade_matches(matches, color)
 
 
 def display_image_with_matches(image):
@@ -64,10 +74,10 @@ if __name__ == '__main__':
     grayscale_image = cv.cvtColor(original_image, cv.COLOR_BGR2GRAY)
 
     if args.frontal:
-        analyze_image(analysis=FRONTAL, image=grayscale_image, classifier=FRONTAL_FACE)
+        analyze_image(analysis=FRONTAL, image=grayscale_image, classifier=FRONTAL_FACE, color=args.color)
 
     if args.profile:
-        analyze_image(analysis=PROFILE, image=grayscale_image, classifier=PROFILE_FACE)
+        analyze_image(analysis=PROFILE, image=grayscale_image, classifier=PROFILE_FACE, color=args.color)
 
     # Show image with matches highlighted in green rectangles
     display_image_with_matches(original_image)
